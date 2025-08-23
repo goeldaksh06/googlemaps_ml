b4aaa3db9c770f6147689acc30437af3055fa4b2
Daksh Goel <goeldaksh06@gmail.com>
2025-08-24 03:32:52 +0530

    Removed extra files, keeping only main.py and synthetic_routes_dataset.csv

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-
-print("✅ Setup complete!")
